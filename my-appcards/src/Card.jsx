0cfde91c59bc342b07258623a69f1e6d0c0b6bd6
Idris-Git <xissi0077x@gmail.com>
2024-07-31 15:37:33 +0400

    first commit

---
+++ my-appcards/src/Card.jsx
@@ -0,0 +1,12 @@
+import profilePic from './assets/insta pic.jpg'
+function Card(){
+    return(
+        <div className="card">
+            <img className='card-image' src={profilePic} alt="profile picture" ></img>
+            <h2 className='card-title'>Idris Said</h2>
+            <p className='card-text'>I'm a student at SoharUni in the FCIT</p>
+        </div>
+    );
+}
+
+export default Card
