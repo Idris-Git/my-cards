2dc10ad5e163a457e4e9ff930dabcf941f439404
Idris-Git <158810861+Idris-Git@users.noreply.github.com>
2025-02-09 21:30:41 +0400

    t1

--- my-appcards/src/Card.jsx
+++ my-appcards/src/Card.jsx
@@ -4,7 +4,7 @@ function Card(){
         <div className="card">
             <img className='card-image' src={profilePic} alt="profile picture" ></img>
             <h2 className='card-title'>Idris Said</h2>
-            <p className='card-text'>I'm a student at SoharUni in the FCIT</p>
+            <p className='card-text'>I'm a student at SoharUni in the FCIT..</p>
         </div>
     );
 }
